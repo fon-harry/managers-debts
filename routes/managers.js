ff0afb4a1043e4fa7ea58be96bcfc3bb915c985b
fon_harry <fon.harry@gmail.com>
2019-04-13 19:25:37 +0300

    Added managers list to manager template

--- routes/managers.js
+++ routes/managers.js
@@ -3,18 +3,22 @@ const router = express.Router();
 
 const managersData = require('../data/data.json');
 
-/* GET managers listing. */
+
 router.get('/', function(req, res, next) {
-    const managersList = [];
-    managersData.forEach(({id, name}) => {
-        managersList.push({id, name, link: `managers/${name}`})
-    });
-    res.json(managersList);
+    res.redirect(`/managers/${managersData[0].name}`);
 });
 
 router.get('/:managerName', function(req, res, next) {
     const managerName = req.params.managerName;
     managerData = managersData.find(item => item['name'] === managerName);
+
+    const managersList = [];
+    managersData.forEach(({id, name}) => {
+        managersList.push({id, name, link: `/managers/${name}`})
+    });
+
+    managerData['managersList'] = managersList;
+
     res.render('manager', managerData);
 });
 
