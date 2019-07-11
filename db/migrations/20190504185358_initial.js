2de76ac87fa8ddf2f2da5edf563f6ae65a12d0c4
fon_harry <fon.harry@gmail.com>
2019-07-11 22:52:51 +0300

    Fixed migrations

--- db/migrations/20190504185358_initial.js
+++ db/migrations/20190504185358_initial.js
@@ -1,34 +1,30 @@
-exports.up = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.createTable("managers", function(table) {
-      table.increments("id").primary();
-      table.uuid("id_8");
-      table.string("name");
-    }),
+exports.up = async function(knex, Promise) {
+  await knex.schema.createTable("managers", function(table) {
+    table.increments("id").primary();
+    table.uuid("id_8");
+    table.string("name");
+  });
 
-    knex.schema.createTable("clients", function(table) {
-      table.increments("id").primary();
-      table.uuid("id_8");
-      table.string("name");
-      table.integer("manager_id").unsigned();
-      table.foreign("manager_id").references("managers.id");
-    }),
+  await knex.schema.createTable("clients", function(table) {
+    table.increments("id").primary();
+    table.uuid("id_8");
+    table.string("name");
+    table.integer("manager_id").unsigned();
+    table.foreign("manager_id").references("managers.id");
+  });
 
-    knex.schema.createTable("contracts", function(table) {
-      table.increments("id").primary();
-      table.uuid("id_8");
-      table.string("name");
-      table.decimal("debt", 14, 2);
-      table.integer("client_id").unsigned();
-      table.foreign("client_id").references("clients.id");
-    })
-  ]);
+  await knex.schema.createTable("contracts", function(table) {
+    table.increments("id").primary();
+    table.uuid("id_8");
+    table.string("name");
+    table.decimal("debt", 14, 2);
+    table.integer("client_id").unsigned();
+    table.foreign("client_id").references("clients.id");
+  });
 };
 
-exports.down = function(knex, Promise) {
-  return Promise.all([
-    knex.schema.dropTable("contracts"),
-    knex.schema.dropTable("clients"),
-    knex.schema.dropTable("managers")
-  ]);
+exports.down = async function(knex, Promise) {
+  await knex.schema.dropTable("contracts");
+  await knex.schema.dropTable("clients");
+  await knex.schema.dropTable("managers");
 };
